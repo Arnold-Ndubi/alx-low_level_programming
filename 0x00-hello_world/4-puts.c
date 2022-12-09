302bd64b3d3352fedae32390f4e914156cb8c499
Arnold-Ndubi <ndubyarnold95@gmail.com>
2022-12-08 17:02:28 -0800

    trial 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,9 @@
 */
 int main(void)
 {
-               char Mystr[] = "\"Programming is like building a multilingual puzzle";
-                                                                
-                                                           	       puts(Mystr);
-							       	       return (0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
+	puts(Mystr);
+       	return (0);
 }
 
