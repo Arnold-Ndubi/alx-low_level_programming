3810aba09eb2211748f8732ef2b9148f5fcee8f1
Arnold-Ndubi <ndubyarnold95@gmail.com>
2022-12-14 08:21:15 -0800

    0 trial 8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-       	char h[] ="_putchar";
-	int i:
-
-	for(i=0;h[i] != '\0'; i++)
-	{
-		_putchar(h[1]):
-	}
-	_putchar("/n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,2 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#include <stdio.h>
 int _putchar(char ch);
 
