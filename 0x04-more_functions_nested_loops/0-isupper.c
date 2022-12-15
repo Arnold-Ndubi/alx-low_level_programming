798a9b588f608e3a67a6eaad07b78ffa08c06f96
Arnold-Ndubi <ndubyarnold95@gmail.com>
2022-12-15 15:42:32 -0800

    number 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,23 +4,17 @@
 * _isupper - check for uppercase character.
 *
 * @c: character
-*
 * Return: 1 if c is uppercase and 0 otherwise.
-*
 */
 
 int _isupper(int c)
 
 {
+	int i;
 
 	if (c >= 65 && c <= 90)
-
-	{
-
-		return (1);
-
-	}
+		i = 1;
 	else
-	{
-	return (0);
+		i = 0;
+	return (i);
 }
