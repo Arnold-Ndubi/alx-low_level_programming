a8dd680e3c81bf97071ec73069d1a94532980a03
Arnold-Ndubi <ndubyarnold95@gmail.com>
2022-12-22 00:10:20 -0800

    trial 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,6 +6,7 @@
  * @s2: string 2
  * Return: result
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
@@ -17,5 +18,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
+		i++;
+	}
 		return (0);
 	}
